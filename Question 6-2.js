2ffb91f62338fbc3327b3b88d2e82dee6d32e211
IbrahimGaber322 <ibrahimseda322@gmail.com>
2023-07-28 04:00:25 +0300

    fixed to make it work for the first 45 not 44

--- Question 6-2.js
+++ Question 6-2.js
@@ -2,7 +2,7 @@ function checkMissing(arr) {
   // Initialize a counter to the first element of the array
   var counter = arr[0];
   // Loop through the array
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length + 1; i++) {
     // If the current element is not equal to the counter, then the missing number is between the previous element and the current element
     if (counter != arr[i]) return arr[i - 1] + 1;
     // Otherwise, increment the counter
